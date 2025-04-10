4889b119e0e43fe21e0c0b90d4c29c925c2d6451
Rafał Uzarowicz <rafal.uzarowicz@gmail.com>
2025-04-11 00:35:12 +0200

    refactor: major changes to how viewer is structured.
    Removed old viewer.
    
    The viewer was split into multiple files.
    
    The player is now configured from a config file.
    
    Fixed old viewer not working based on Twitch API changes.
    
    Added classes and moved all the logic to those classes.
    
    The description now uses a single div.
    
    Added aliases functionality.
    
    Added banned title words functionality.

---
+++ common.js
@@ -0,0 +1,37 @@
+﻿const CONFIG_FILE_PATH = 'config.txt'
+
+const BROADCAST_CHANNEL_NAME = "clip_player_description_sync"
+
+async function fileExists(url) {
+    try {
+        const response = await fetch(url, { method: "HEAD" });
+        return response.ok;
+    } catch (error) {
+        return false;
+    }
+}
+
+async function readGlobalConfig() {
+    if (!await fileExists(CONFIG_FILE_PATH)){
+        console.error("Config file missing: " + CONFIG_FILE_PATH)
+    }
+
+    let ret = {};
+
+    let text = await (await fetch(CONFIG_FILE_PATH)).text();
+
+    const lines = text.split('\n');
+
+    for (let line of lines) {
+        const s = line.split(/=(.*)/s)
+
+        if (s.length < 2) continue;
+
+        const key = s[0].trim().replace(/\r/g, "");
+        const value = s[1].trim().replace(/\r/g, "");
+
+        ret[key] = value;
+    }
+
+    return ret;
+}
